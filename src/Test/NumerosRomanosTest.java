92b347fee381d78acaeb3af0be79e4c453f8b8c7
ancabdia <ancabdia.contact@gmail.com>
2021-07-08 21:48:39 +0100

    Test para casos bases

--- src/Test/NumerosRomanosTest.java
+++ src/Test/NumerosRomanosTest.java
@@ -13,4 +13,39 @@ class NumerosRomanosTest {
 
         assertEquals(romano, "I");
     }
+
+    @org.junit.jupiter.api.Test
+    void pasar2Aromanos() {
+        String romano = numerosRomanos.naturalAromano(2);
+
+        assertEquals(romano, "II");
+    }
+
+    @org.junit.jupiter.api.Test
+    void pasar3Aromanos() {
+        String romano = numerosRomanos.naturalAromano(3);
+
+        assertEquals(romano, "III");
+    }
+
+    @org.junit.jupiter.api.Test
+    void pasar4Aromanos() {
+        String romano = numerosRomanos.naturalAromano(4);
+
+        assertEquals(romano, "IV");
+    }
+
+    @org.junit.jupiter.api.Test
+    void pasar5Aromanos() {
+        String romano = numerosRomanos.naturalAromano(5);
+
+        assertEquals(romano, "V");
+    }
+
+    @org.junit.jupiter.api.Test
+    void pasar6Aromanos() {
+        String romano = numerosRomanos.naturalAromano(6);
+
+        assertEquals(romano, "VI");
+    }
 }
--- src/main/NumerosRomanos.java
+++ src/main/NumerosRomanos.java
@@ -3,6 +3,13 @@ package main;
 public class NumerosRomanos {
 
     public String naturalAromano(int numeroNatural){
-        return "I";
+
+        switch (numeroNatural){
+            case 1: return "I";
+            case 2: return "II";
+            case 3: return "III";
+            case 4: return "IV";
+        }
+        return null;
     }
 }
