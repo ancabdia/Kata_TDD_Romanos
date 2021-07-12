adf9cf955914364a5c7dd362a1010c704205dc1f
ancabdia <ancabdia.contact@gmail.com>
2021-07-12 21:53:57 +0100

    Refactor y merch de pasarUnidad() y pasarDecena()

--- src/main/NumerosRomanos.java
+++ src/main/NumerosRomanos.java
@@ -12,39 +12,24 @@ public class NumerosRomanos {
         char[] numerosChar = numeroNatural.toString().toCharArray(); //almacenar en []
 
         if(numerosChar.length >= 2){ //separar unidades mayor a 1
-            String unidadRomana =  pasarUnidad(Integer.parseInt(String.valueOf(numerosChar[1])));
-            String decenaRomana = pasarDecena(Integer.parseInt(String.valueOf(numerosChar[0])));
+            String unidadRomana =  pasarUnidades(Integer.parseInt(String.valueOf(numerosChar[1])), 0, 1, 2);
+            String decenaRomana = pasarUnidades(Integer.parseInt(String.valueOf(numerosChar[0])), 2, 3 ,4);
 
             return decenaRomana+unidadRomana;
         }else{
-            return pasarUnidad(Integer.parseInt(String.valueOf(numerosChar[0])));
+            return pasarUnidades(Integer.parseInt(String.valueOf(numerosChar[0])),0, 1,2);
         }
     }
 
-    private String pasarDecena(int decena) {
+    private String pasarUnidades(int decena, int x, int y, int z) {
         switch (decena){
-            case 4: return simbolosRomanos.get(2) + simbolosRomanos.get(3);
-            case 9: return simbolosRomanos.get(2) + simbolosRomanos.get(4);
+            case 4: return simbolosRomanos.get(x) + simbolosRomanos.get(y);
+            case 9: return simbolosRomanos.get(x) + simbolosRomanos.get(z);
         }
 
-        if (decena <= 3) return sumar(1, decena, "", simbolosRomanos.get(2));
+        if (decena <= 3) return sumar(1, decena, "", simbolosRomanos.get(x));
 
-        if (decena <= 8) return sumar(6, decena, simbolosRomanos.get(3), simbolosRomanos.get(2));
-
-
-        return null;
-    }
-
-    private String pasarUnidad(int unidad) {
-        switch (unidad){
-            case 4: return simbolosRomanos.get(0) + simbolosRomanos.get(1);
-            case 9: return simbolosRomanos.get(0) + simbolosRomanos.get(2);
-        }
-
-        if (unidad <= 3) return sumar(1, unidad, "", simbolosRomanos.get(0));
-
-        if (unidad <= 8) return sumar(6, unidad, simbolosRomanos.get(1), simbolosRomanos.get(0));
-
+        if (decena <= 8) return sumar(6, decena, simbolosRomanos.get(y), simbolosRomanos.get(x));
 
         return null;
     }
